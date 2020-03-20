9ddde942e47f89db03fac2e4d8bc952181d77505
atabaosares07 <atabaosares07@gmail.com>
2020-03-20 23:33:00 +0800

    Add ILogger interface

---
+++ Book.Logger/ILoggerProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Book.Logger
+{
+    public interface ILogger
+    {
+        string Log(string message);
+    }
+}
